748b476b1b599397443532f8225045ec0a124074
Melissa González <melissa.gonzalez.nebrijo@gmail.com>
2024-06-16 19:28:23 -0500

    Mapping models using attributes

---
+++ Models/Task.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace minimal_api_ef.Models;
+
+public class Task
+{
+    [Key]
+    public Guid TaskId { get; set; }
+    [ForeignKey("CategoryId")]
+    public Guid CategoryId { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Priority TaskPriority { get; set; }
+    public DateTime CreationDate { get; set; }
+    public virtual Category Category { get; set; }
+    public string Summary { get; set; }
+}
+
+public enum Priority
+{
+    Low,
+    Medium,
+    High
+}
---
+++ TaskContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using minimal_api_ef.Models;
+using Task = minimal_api_ef.Models.Task;
+
+namespace minimal_api_ef;
+
+public class TaskContext: DbContext
+{
+    public DbSet<Category> Categories { get; set;  }
+    public DbSet<Task> Tasks { get; set; }
+    public TaskContext(DbContextOptions<TaskContext> options) : base(options) {}
+}
