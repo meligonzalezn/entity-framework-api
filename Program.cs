ea227947f333a9a012b1352b592820572c759c64
Melissa González <melissa.gonzalez.nebrijo@gmail.com>
2024-06-16 19:49:05 -0500

    Using databases in memory

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using minimal_api_ef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TaskContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// code to create database
+app.MapGet("/dbconnection", async ([FromServices] TaskContext dbContext) =>
+{
+    // Ensure database is created 
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Database in memory: " + dbContext.Database.IsInMemory());
+});
 app.Run();
